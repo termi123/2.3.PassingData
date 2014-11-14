c413273bc37cb9e21f3c4df59326b38cb9355db8
termi123 <dung.phantu@gmail.com>
2014-11-14 14:31:49 +0700

    create

---
+++ src/net/learn2develop/PassingData/Test1.java
@@ -0,0 +1,12 @@
+package net.learn2develop.PassingData;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test1 extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/net/learn2develop/PassingData/Test12.java
@@ -0,0 +1,15 @@
+package net.learn2develop.PassingData;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Test12 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test12);
+	}
+}
